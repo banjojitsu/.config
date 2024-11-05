8c61705a1de9b045012f3a37ebf1d4916a38adae
jason <>
2024-11-05 12:55:28 -0500

    add treesitter with incremental selection

---
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,20 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  main = 'nvim-treesitter.configs',
+  opts = {
+    ensure_installed = {"lua"},
+    auto_install = true,
+    highlight = {enable=true},
+    indent = {enable=true},
+    incremental_selection = {
+      enable=true,
+      keymaps = {
+        init_selection = '<cr>',
+        scope_incremental = '<cr>',
+        node_incremental = '<tab>',
+        node_decremental = '<s-tab>'
+      }
+    }
+  }
+}
