c6da3097f09d0ea1ee086a58928f74bd74199796
jason <>
2024-11-05 08:38:33 -0500

    add neovim option configs

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,17 +1,17 @@
 vim.g.have_nerd_font = true
+vim.g.netrw_banner = false
+vim.g.netrw_liststyle = 3
 
--- Make line numbers default
-vim.opt.number = true
-
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
--- Sets how neovim will display certain whitespace characters in the editor.
-vim.opt.list = true
+vim.opt.cursorline = true -- Show which line the cursos is on
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.list = true -- Set how neovim displays certain whitespace
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 4
+vim.opt.number = true -- Make line numbers default
+vim.opt.scrolloff = 4 -- Lines of context
+vim.opt.shiftround = true -- Round indent
+vim.opt.shiftwidth = 2 -- Size of an indent
+vim.opt.signcolumn = "yes" -- For gitsigns
+vim.opt.smartindent = true -- Inserts indents automatically
+vim.opt.tabstop = 2 -- Number of spaces tabs count for
+vim.opt.termguicolors = true -- True color support
+vim.opt.timeoutlen = 300 -- Displays which-key popup sooner
